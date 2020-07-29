dc9343f63e3df73b98b40409b975cd3ca9aee79a
Burniel123 <dgburton12@gmail.com>
2020-07-29 13:01:39 +0100

    And functionality and logic. All components apart from the submit button now work. Program correctly computes the answer and logs it.

--- Assets/doubleListeningScript.cs
+++ Assets/doubleListeningScript.cs
@@ -24,8 +24,16 @@ public class doubleListeningScript : MonoBehaviour
 	public AudioSource sound2;
 
 	public AudioClip[] sounds;
-	public String[] soundNames = new String[] {"Arcade","BallpointPenWriting","Beach","BookPageTurning","CarEngine","Casino","CensorshipBleep","Chainsaw","CompressedAir","Cow","DialupInternet","DoorClosing","ExtractorFan","FireworkExploding","GlassShattering","Helicopter","Marimba","MedievalWeapons","Oboe","PhoneRinging","PoliceRadioScanner","RattlingIronChain","ReloadingGlock19","Saxophone","ServoMotor","SewingMachine","SoccerMatch","SqueakyToy","Supermarket","TableTennis","TawnyOwl","TaxiDispatch","TearingFabric","ThroatSinging","ThrushNightingale","TibetanNuns","TrainStation","Tuba","VacuumCleaner","Waterfall","Zipper"};
-	bool moduleSolved = false;
+	public String[] soundNames = new String[] {"Arcade","Ballpoint Pen Writing","Beach","Book Page Turning","Car Engine","Casino","Censorship Bleep","Chainsaw","Compressed Air","Cow","Dialup Internet","Door Closing","Extractor Fan","Firework Exploding","Glass Shattering","Helicopter","Marimba","Medieval Weapons","Oboe","Phone Ringing","Police Radio Scanner",
+	"Rattling Iron Chain","Reloading Glock 19","Saxophone","Servo Motor","Sewing Machine","Soccer Match","Squeaky Toy","Supermarket","Table Tennis","Tawny Owl","Taxi Dispatch","Tearing Fabric","Throat Singing","Thrush Nightingale","Tibetan Nuns","Train Station","Tuba","Vacuum Cleaner","Waterfall","Zipper"};
+	private bool moduleSolved = false;
+	private Dictionary<String, String> listeningCodes = new Dictionary<String, String>()
+	{
+		{"Taxi Dispatch","&&&**"},{"Cow","&$#$&"},{"Extractor Fan","$#$*&"},{"Train Station","#$$**"},{"Arcade","$#$#*"},{"Casino","**$*#"},{"Supermarket","#$$&*"},{"Soccer Match","##*$*"},{"Tawny Owl","$#*$*"},{"Sewing Machine","#&&*#"},{"Thrush Nightingale","**#**"},{"Car Engine","&#**&"},{"Reloading Glock 19","$&**#"},{"Oboe","&#$$#"},
+		{"Saxophone","$&&**"},{"Tuba","#&$##"},{"Marimba","&*$*$"},{"Phone Ringing","&$$&*"},{"Tibetan Nuns","#&&&&"},{"Throat Singing","**$$$"},{"Beach","*&*&&"},{"Dialup Internet","*#&*&"},{"Police Radio Scanner","**###"},{"Censorship Bleep","&&$&*"},{"Medieval Weapons","&$**&"},{"Door Closing","#$#&$"},{"Chainsaw","&#&&#"},
+		{"Compressed Air","$$*$*"},{"Servo Motor","$&#$$"},{"Waterfall","&**$$"},{"Tearing Fabric","$&&*&"},{"Zipper","&$&##"},{"Vacuum Cleaner","#&$*&"},{"Ballpoint Pen Writing","$*$**"},{"Rattling Iron Chain","*#$&&"},{"Book Page Turning","###&$"},{"Table Tennis","*$$&$"},{"Squeaky Toy","$*&##"},{"Helicopter","#&$&&"},{"Firework Exploding","$&$$*"},{"Glass Shattering","*$*$*"}
+	};
+
 	bool soundsPlaying = false;
 	int[] soundPositions;
 
@@ -41,24 +49,104 @@ public class doubleListeningScript : MonoBehaviour
 		KMSelectable pb = playButton;
 		pb.OnInteract += delegate(){PressPlay(); return false;};
 
-		foreach(KMSelectable up in upArrows)
+		for(int i = 0; i < upArrows.Length; i++)
 		{
-			KMSelectable pressedArrow = up;
-			//up.OnInteract += delegate(){PressNumber(pressedNumber); return false;};
+			int j = i;
+			upArrows[i].OnInteract += delegate(){PressUp(j); return false;};
 		}
-		foreach(KMSelectable down in downArrows)
+		for(int i = 0; i < downArrows.Length; i++)
 		{
-			KMSelectable pressedArrow = down;
-			//down.OnInteract += delegate(){PressNumber(pressedNumber); return false;};
+			int j = i;
+			downArrows[i].OnInteract += delegate(){PressDown(j); return false;};
 		}
 	}
-
 
 	//Initialize module.
 	void Start() 
 	{
 		soundPositions = Enumerable.Range(0, 41).ToList().Shuffle().Take(2).ToArray();
-		Debug.LogFormat("[Double Listening #{0}] The chosen sounds are {1} and {2}.", moduleId, soundNames[soundPositions[0]], soundNames[soundPositions[1]]);
+		Debug.LogFormat("[Double Listening #{0}] The chosen sounds are \"{1}\" and \"{2}\".", moduleId, soundNames[soundPositions[0]], soundNames[soundPositions[1]]);
+		String listeningCode1 = listeningCodes[soundNames[soundPositions[0]]];
+		String listeningCode2 = listeningCodes[soundNames[soundPositions[1]]];
+		Debug.LogFormat("[Double Listening #{0}] The listening code for \"{1}\" is {2}", moduleId, soundNames[soundPositions[0]], listeningCode1);
+		Debug.LogFormat("[Double Listening #{0}] The listening code for \"{1}\" is {2}", moduleId, soundNames[soundPositions[1]], listeningCode2);
+		String solution = CalculateSolution(listeningCode1,listeningCode2);
+		Debug.LogFormat("[Double Listening #{0}] Solution: {1}.", moduleId, solution);
+	}
+
+	//Given two listening codes, calculates a String of binary to input to solve the module.
+	String CalculateSolution(String symbols1, String symbols2)
+	{
+		int rowNum = DetermineTableRowNum();
+		int[] mappings = new int[4];
+		switch(rowNum)
+		{
+			case 0 :
+			Debug.LogFormat("[Double Listening #{0}] Using table row 1: \"If the bomb has at least 3 batteries and at least one of the sounds was Beach or Waterfall\".", moduleId);
+			mappings = new int[] {0,0,1,1};break;
+			case 1 :
+			Debug.LogFormat("[Double Listening #{0}] Using table row 2: \"Otherwise, if the bomb has an empty port plate\".", moduleId);
+			mappings = new int[] {0,1,0,1};break;
+			case 2 :
+			Debug.LogFormat("[Double Listening #{0}] Using table row 3: \"Otherwise, if the last digit of the bomb's serial number is odd\".", moduleId);
+			mappings = new int[] {1,0,1,0};break;
+			case 3 :
+			Debug.LogFormat("[Double Listening #{0}] Using table row 4: \"Otherwise\".", moduleId);
+			mappings = new int[] {1,1,0,0};break;
+		}
+		
+		String binary1 = ObtainBinaryOperand(symbols1, mappings);
+		String binary2 = ObtainBinaryOperand(symbols2, mappings);
+		Debug.LogFormat("[Double Listening #{0}] The binary code for \"{1}\" is {2}.", moduleId, symbols1, binary1);
+		Debug.LogFormat("[Double Listening #{0}] The binary code for \"{1}\" is {2}.", moduleId, symbols2, binary2);
+
+		return BitwiseXor(binary1, binary2);
+	}
+
+	//Performs edgework lookups to determine which row of the table (0-3, top to bottom) to use.
+	int DetermineTableRowNum()
+	{
+		String listeningName1 = soundNames[soundPositions[0]];
+		String listeningName2 = soundNames[soundPositions[1]];
+		if(Bomb.GetBatteryCount() >= 3 && (listeningName1.Equals("Beach") || listeningName1.Equals("Waterfall") || listeningName2.Equals("Beach") || listeningName2.Equals("Waterfall")))
+			return 0;
+		foreach(var plate in Bomb.GetPortPlates())
+		{
+			if(plate.Count() == 0)
+				return 1;
+		}
+		if(int.Parse(Bomb.GetSerialNumber().Substring(5,1)) % 2 == 1)
+			return 2;
+		return 3;
+	}
+
+	//Maps a String of listening characters to a String of binary.
+	String ObtainBinaryOperand(String symbols, int[] mappings)
+	{
+		String binary = "";
+
+		foreach(char c in symbols)
+		{
+			if(c == '#') binary += mappings[0].ToString();
+			else if(c == '$') binary += mappings[1].ToString();
+			else if(c == '&') binary += mappings[2].ToString();
+			else binary += mappings[3].ToString();
+		}
+
+		return binary;
+	}
+
+	//Performs a simple bitwise XOR on strings (using this over a library method because 5 bits is awkward).
+	String BitwiseXor(String op1, String op2)
+	{
+		String result = "";
+		for(int i = 0; i < op1.Length; i++)
+		{
+			if(op1[i] != op2[i]) result += "1";
+			else result += "0";
+		}
+
+		return result;
 	}
 
 	//Process the user pressing the "play" button.
@@ -68,6 +156,28 @@ public class doubleListeningScript : MonoBehaviour
 			return;
 
 		StartCoroutine(PlaySounds(soundPositions));
+	}
+
+	//Respond to the user pressing an up arrow by changing that bit to a 1 (regardless of its current value).
+	void PressUp(int arrowNum)
+	{
+		if(moduleSolved)
+			return;
+
+		bitDisplays[arrowNum].GetComponentInChildren<TextMesh>().text = "1\n";
+		upArrows[arrowNum].AddInteractionPunch(0.5f);
+		Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+	}
+
+	//Respond to the user pressing a down arrow by changing that bit to a 0 (regardless of its current value).
+	void PressDown(int arrowNum)
+	{
+		if(moduleSolved)
+			return;
+		
+		bitDisplays[arrowNum].GetComponentInChildren<TextMesh>().text = "0\n";
+		downArrows[arrowNum].AddInteractionPunch(0.5f);
+		Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
 	}
 
 	//Play both sounds and run a cooldown of 5 seconds.
